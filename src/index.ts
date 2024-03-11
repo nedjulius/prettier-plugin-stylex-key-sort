328c20b556234e2450c6dd267b8e080c78343f45
nedjulius <julius.nedzinskas@gmail.com>
2024-03-10 22:15:40 -0400

    Add custom traverse

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
+import { AstPath, Doc, Parser, ParserOptions } from 'prettier';
 import * as parserBabel from 'prettier/plugins/babel';
 
+function withStylexKeySort(parser: Parser): Parser {
+  return {
+    ...parser,
+    parse: function(text: string, options: ParserOptions) {
+      return traverse(parser.parse(text, options));
+    },
+  };
+}
+
+function traverse(node: any) {
+  console.log(node, 'node');
+
+  return node;
+}
+
+export const languages = [
+  {
+    name: 'JavaScript',
+    parsers: ['babel'],
+    extensions: ['.js', '.jsx'],
+  },
+];
+
 export const parsers = {
-  babel: parserBabel.parsers.babel,
+  babel: withStylexKeySort(parserBabel.parsers.babel),
 };
