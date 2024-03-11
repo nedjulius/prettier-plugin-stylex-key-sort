da2d01d53ef54c116c5caacb120ee6c87f9af259
nedjulius <julius.nedzinskas@gmail.com>
2024-03-11 00:21:51 -0400

    Add state and variable traversal

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,92 @@
-import { AstPath, Doc, Parser, ParserOptions } from 'prettier';
+import {
+  ImportDeclaration,
+  Literal,
+  Program,
+  VariableDeclarator,
+} from 'estree';
+import { Parser, ParserOptions } from 'prettier';
 import * as parserBabel from 'prettier/plugins/babel';
 
 function withStylexKeySort(parser: Parser): Parser {
   return {
     ...parser,
     parse: function(text: string, options: ParserOptions) {
-      return traverse(parser.parse(text, options));
+      const ast = parser.parse(text, options);
+
+      stylexKeySort(ast.program);
+
+      return ast;
     },
   };
 }
 
-function traverse(node: any) {
-  console.log(node, 'node');
+function stylexKeySort(program: Program) {
+  const stylexNamespace = new Set<string>();
+  const stylexCreate = new Set<string>();
+  const stylexKeyframes = new Set<string>();
 
-  return node;
+  const stylexImports = program.body.filter(
+    (node): node is ImportDeclaration =>
+      node.type === 'ImportDeclaration' && isStylexImportSource(node.source),
+  );
+
+  if (stylexImports.length === 0) {
+    // terminate if there are no stylex imports
+    return;
+  }
+
+  stylexImports.forEach((node) => {
+    node.specifiers.forEach((specifier) => {
+      if (
+        specifier.type === 'ImportDefaultSpecifier' ||
+        specifier.type === 'ImportNamespaceSpecifier'
+      ) {
+        stylexNamespace.add(specifier.local.name);
+      }
+
+      if (
+        specifier.type === 'ImportSpecifier' &&
+        specifier.imported.name === 'create'
+      ) {
+        stylexCreate.add(specifier.local.name);
+      }
+
+      if (
+        specifier.type === 'ImportSpecifier' &&
+        specifier.imported.name === 'keyframes'
+      ) {
+        stylexKeyframes.add(specifier.local.name);
+      }
+    });
+  });
+
+  for (const node of program.body) {
+    if (node.type === 'VariableDeclaration') {
+      // this is crazy, will refactor
+      // just for testing right now
+      //
+      // i really dont want to use traverse due to performance
+      // but i can probably utilize @babel/types
+      const sortable = node.declarations.filter(
+        (declaration): declaration is VariableDeclarator =>
+          declaration.type === 'VariableDeclarator' &&
+          declaration.init?.type === 'CallExpression' &&
+          ((declaration.init.callee.type === 'Identifier' &&
+            stylexCreate.has(declaration.init.callee.name)) ||
+            (declaration.init.callee.type === 'MemberExpression' &&
+              declaration.init.callee.object.type === 'Identifier' &&
+              declaration.init.callee.property.type === 'Identifier' &&
+              stylexNamespace.has(declaration.init.callee.object.name) &&
+              declaration.init.callee.property.name === 'create')),
+      );
+
+      console.log(sortable, 'sortable');
+    }
+  }
+}
+
+function isStylexImportSource(source: Literal) {
+  return source.value === '@stylexjs/stylex' || source.value === 'stylex';
 }
 
 export const languages = [
